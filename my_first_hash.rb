18c70ec35477875adea14cfb669754ed166dee4b
Ryan Zemlin <rzemlin@gmail.com>
2021-02-17 13:42:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,10 +3,6 @@ def my_hash
 end
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-  
-end
-
-
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
